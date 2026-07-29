56b682982d046da7bab0ca339b01a91190cae5c6
Joey <joey@evolution-x.org>
2026-07-30 01:40:07 +0900

    gms: Update from komodo BP1A.250305.020

---
+++ product/packages/privileged_apps/SCONE/Android.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2023 Raphielscape LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the License);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter blueline crosshatch sargo bonito flame coral sunfish bramble redfin barbet oriole raven bluejay panther cheetah lynx tangorpro felix shiba husky akita tokay caiman komodo comet frankel blazer mustang rango, $(LINEAGE_BUILD)),)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := SCONE
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := SCONE.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRODUCT_MODULE := true
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_OPTIONAL_USES_LIBRARIES := android.telephony.satellite org.apache.http.legacy androidx.window.extensions androidx.window.sidecar
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
+
+endif
